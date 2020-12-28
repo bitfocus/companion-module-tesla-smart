26edf5ed747d50fe3b4ccec331dc1cefff9a7f6c
Bjorn Peterson <bjkarl@yahoo.com>
2020-12-28 03:08:49 -0600

    Overhauled this module to support TESmart KVM devices with variable numbers of source inputs, also added button feedback for currently selected source.

--- index.js
+++ index.js
@@ -1,159 +1,359 @@
+var tcp           = require('../../tcp');
 var instance_skel = require('../../instance_skel');
-var tcp = require('../../tcp');
 var debug;
 var log;
 
 function instance(system, id, config) {
-		var self = this;
+	var self = this;
 
-		// super-constructor
-		instance_skel.apply(this, arguments);
-		self.actions(); // export actions
-		return self;
+	// super-constructor
+	instance_skel.apply(this, arguments);
+
+	self.actions(); // export actions
+
+	return self;
 }
 
-instance.prototype.init = function () {
-		var self = this;
+instance.prototype.CHOICES_PC = [
+	{ id: '1', label: 'Source 1' },
+	{ id: '2', label: 'Source 2' },
+	{ id: '3', label: 'Source 3' },
+	{ id: '4', label: 'Source 4' },
+	{ id: '5', label: 'Source 5' },
+	{ id: '6', label: 'Source 6' },
+	{ id: '7', label: 'Source 7' },
+	{ id: '8', label: 'Source 8' },
+	{ id: '9', label: 'Source 9' },
+	{ id: '10', label: 'Source 10' },
+	{ id: '11', label: 'Source 11' },
+	{ id: '12', label: 'Source 12' },
+	{ id: '13', label: 'Source 13' },
+	{ id: '14', label: 'Source 14' },
+	{ id: '15', label: 'Source 15' },
+	{ id: '16', label: 'Source 16' },
+];
 
-		debug = self.debug;
-		log = self.log;
+instance.prototype.init = function() {
+	var self = this;
 
-		self.status(self.STATUS_UNKNOWN);
+	debug = self.debug;
+	log = self.log;
 
-		if (self.config.host !== undefined) {
-			self.tcp = new tcp(self.config.host, 5000);
+	self.current_host_id = '0';
+	self.polling = false;
+	self.choices = self.CHOICES_PC.slice(0, self.config.sources);
 
-			self.tcp.on('status_change', function (status, message) {
-				self.status(status, message);
-			});
+	self.actions();
 
-			self.tcp.on('error', function () {
-				// Ignore
-			});
-		}
+	// Connect to KVM device
+	self.init_tcp();
+
+	// Start polling for feedback
+	self.start_polling_for_host();
+
+	// Initialize the feedback workflow
+	self.initFeedbacks();
 };
 
-instance.prototype.updateConfig = function (config) {
-		var self = this;
-		self.config = config;
+// Reconnect to KVM after config updated, in case of IP or port change
+instance.prototype.updateConfig = function(config) {
+	var self = this;
 
-		if (self.tcp !== undefined) {
-			self.tcp.destroy();
-			delete self.tcp;
-		}
+	if (self.socket !== undefined) {
+		self.socket.destroy();
+		delete self.socket;
+	}
 
-		if (self.config.host !== undefined) {
-			self.tcp = new tcp(self.config.host, 5000);
+	self.config = config;
+	self.choices = self.CHOICES_PC.slice(0, self.config.sources);
 
-			self.tcp.on('status_change', function (status, message) {
-				self.status(status, message);
-			});
+	self.actions();
+	self.initFeedbacks();
 
-			self.tcp.on('error', function (message) {
-				// ignore for now
-			});
-		}
+	self.init_tcp();
+
+	self.stop_polling_for_host();
+	self.start_polling_for_host();
 };
+
+instance.prototype.init_tcp = function() {
+	var self = this;
+
+	if (self.socket !== undefined) {
+		self.socket.destroy();
+		delete self.socket;
+	}
+
+	self.status(self.STATE_WARNING, 'Connecting');
+
+	if (self.config.host) {
+		self.socket = new tcp(self.config.host, self.config.port);
+
+		self.socket.on('status_change', function (status, message) {
+			self.status(status, message);
+		});
+
+		self.socket.on('error', function (err) {
+			debug("Network error", err);
+			self.status(self.STATE_ERROR, err);
+			self.log('error',"Network error: " + err.message);
+		});
+
+		self.socket.on('connect', function () {
+			self.status(self.STATE_OK);
+			debug("Connected");
+		})
+
+		self.socket.on('data', function (chunk) {
+			var data = Buffer.from(chunk);
+			debug("Received ", data);
+
+			self.processData(data);
+		});
+	}
+};
+
 
 // Return config fields for web config
 instance.prototype.config_fields = function () {
-		var self = this;
-		return [
-			{
-				type: 'text',
-				id: 'info',
-				width: 12,
-				label: 'Information',
-				value: 'This module is for Tesla Smart KVM Switch 8>1'
-			},
-			{
-				type: 'textinput',
-				id: 'host',
-				label: 'Target IP',
-				width: 6,
-				default: '192.168.1.10',
-				regex: self.REGEX_IP
-			}
-		]
+	var self = this;
+
+	return [
+		{
+			type: 'textinput',
+			id: 'host',
+			label: 'KVM IP',
+			width: 6,
+			regex: self.REGEX_IP
+		},
+		{
+			type: 'textinput',
+			id: 'port',
+			label: 'Target Port',
+			width: 2,
+			default: 5000,
+			regex: self.REGEX_PORT
+		},
+		{
+			type: 'number',
+			id: 'sources',
+			label: '# of sources',
+			min: 1,
+			max: 16,
+			default: 8,
+			required: true
+		},
+		{
+			type: 'number',
+			id: 'poll',
+			label: 'Feedback poll interval (ms) - 0 to disable, max 60000',
+			min: 0,
+			max: 60000,
+			default: 5000,
+			required: true
+		}
+	]
 };
 
 // When module gets deleted
-instance.prototype.destroy = function () {
+instance.prototype.destroy = function() {
 	var self = this;
 
-		if (self.tcp !== undefined) {
-			self.tcp.destroy();
-		}
-		debug("destroy", self.id);
+	self.stop_polling_for_host();
+
+	if (self.socket !== undefined) {
+		self.socket.destroy();
+	}
+
+	debug("destroy", self.id);;
 };
 
-instance.prototype.CHOICES_SOURCES = [
-	{ label: 'Source 1', id: '1' },
-	{ label: 'Source 2', id: '2' },
-	{ label: 'Source 3', id: '3' },
-	{ label: 'Source 4', id: '4' },
-	{ label: 'Source 5', id: '5' },
-	{ label: 'Source 6', id: '6' },
-	{ label: 'Source 7', id: '7' },
-	{ label: 'Source 8', id: '8' }
-];
-
-instance.prototype.actions = function (system) {
+instance.prototype.actions = function() {
 	var self = this;
 
-	var actions = {
+	self.system.emit('instance_actions', self.id, {
 		'switch': {
-			label: 'Switch to source',
-			options: [{
-				type: 'dropdown',
-				label: 'source',
-				id: 'source',
-				default: '1',
-				choices: self.CHOICES_SOURCES
-			}]
+			label: 'Switch source',
+			options: [
+				{
+					type: 'dropdown',
+					id: 'id_send',
+					label: 'Source:',
+					default: '1',
+					choices: self.choices
+				}
+			]
 		}
-	};
-		self.setActions(actions);
-};
+	});
+}
 
-
-instance.prototype.action = function (action) {
+instance.prototype.action = function(action) {
 	var self = this;
-	var id = action.action;
-	var opt = action.options;
 	var cmd;
 
-	switch (id) {
+	switch(action.action) {
 
 		case 'switch':
-			if (opt.source == '1') {
+			var id = action.options.id_send;
+			if(id == '1') {
 				cmd = Buffer.from([0xAA,0xBB,0x03,0x01,0x01,0xEE]);
-			} else if (opt.source == '2') {
+			} else if(id == '2') {
 				cmd = Buffer.from([0xAA,0xBB,0x03,0x01,0x02,0xEE]);
-			} else if (opt.source == '3') {
+			} else if(id == '3') {
 				cmd = Buffer.from([0xAA,0xBB,0x03,0x01,0x03,0xEE]);
-			} else if (opt.source == '4') {
+			} else if(id == '4') {
 				cmd = Buffer.from([0xAA,0xBB,0x03,0x01,0x04,0xEE]);
-			} else if (opt.source == '5') {
+			} else if(id == '5') {
 				cmd = Buffer.from([0xAA,0xBB,0x03,0x01,0x05,0xEE]);
-			} else if (opt.source == '6') {
+			} else if(id == '6') {
 				cmd = Buffer.from([0xAA,0xBB,0x03,0x01,0x06,0xEE]);
-			} else if (opt.source == '7') {
+			} else if(id == '7') {
 				cmd = Buffer.from([0xAA,0xBB,0x03,0x01,0x07,0xEE]);
-			} else if (opt.source == '8') {
+			} else if(id == '8') {
 				cmd = Buffer.from([0xAA,0xBB,0x03,0x01,0x08,0xEE]);
+			} else if(id == '9') {
+				cmd = Buffer.from([0xAA,0xBB,0x03,0x01,0x09,0xEE]);
+			} else if(id == '10') {
+				cmd = Buffer.from([0xAA,0xBB,0x03,0x01,0x0A,0xEE]);
+			} else if(id == '11') {
+				cmd = Buffer.from([0xAA,0xBB,0x03,0x01,0x0B,0xEE]);
+			} else if(id == '12') {
+				cmd = Buffer.from([0xAA,0xBB,0x03,0x01,0x0C,0xEE]);
+			} else if(id == '13') {
+				cmd = Buffer.from([0xAA,0xBB,0x03,0x01,0x0D,0xEE]);
+			} else if(id == '14') {
+				cmd = Buffer.from([0xAA,0xBB,0x03,0x01,0x0E,0xEE]);
+			} else if(id == '15') {
+				cmd = Buffer.from([0xAA,0xBB,0x03,0x01,0x0F,0xEE]);
+			} else if(id == '16') {
+				cmd = Buffer.from([0xAA,0xBB,0x03,0x01,0x10,0xEE]);
 			}
 			break;
-
 	}
 
 	if (cmd !== undefined) {
-		if (self.tcp !== undefined) {
-			debug('sending ', cmd, "to", self.tcp.host);
-			self.tcp.send(cmd);
+		debug('sending ',cmd,"to",self.config.host);
+
+		if (self.socket !== undefined && self.socket.connected) {
+			self.socket.send(cmd);
+		}
+		else {
+			debug('Socket not connected :(');
 		}
 	}
-};
+}
+
+instance.prototype.initFeedbacks = function() {
+	var self = this;
+
+	var feedbacks = {};
+
+	feedbacks["current_host"] = {
+		label : "Currently selected source",
+		description : "Feedback showing the currently selected source",
+		options : [
+			{
+				type : "colorpicker",
+				label : "Background color",
+				id : "bg_color",
+				default : self.rgb(0, 255, 0)
+			},
+			{
+				type : "dropdown",
+				label : "Source",
+				id : "host_id",
+				default : '1',
+				choices : self.choices
+			}
+		]
+	}
+
+	self.setFeedbackDefinitions(feedbacks);
+}
+
+instance.prototype.feedback = function(feedback) {
+	switch (feedback.type) {
+		case "current_host":
+			if (feedback.options.host_id == this.current_host_id) {
+				return {bgcolor : feedback.options.bg_color};
+			}
+	}
+}
+
+instance.prototype.poll_for_active_host = function() {
+	var self = this;
+
+	debug('Polling KVM ',self.config.host);
+
+	if (self.socket !== undefined && self.socket.connected) {
+		self.socket.send(Buffer.from([0xAA,0xBB,0x03,0x10,0x00,0xEE]));
+	}
+	else {
+		debug('Socket not connected :(');
+	}
+
+	this.checkFeedbacks("current_host");
+}
+
+instance.prototype.start_polling_for_host = function() {
+	var self = this;
+
+	if(self.config.poll > 0) {
+		self.host_poller = setInterval(self.poll_for_active_host.bind(self), self.config.poll);
+		self.polling = true;
+
+		self.poll_for_active_host();
+		self.checkFeedbacks("current_host");
+	}
+}
+
+instance.prototype.stop_polling_for_host = function() {
+	var self = this;
+
+	if(self.polling) {
+		clearInterval(self.host_poller);
+	}
+}
+
+instance.prototype.processData = function(data) {
+	var self = this;
+
+	if(data.compare(Buffer.from([0xAA,0xBB,0x03,0x11,0x00,0x16])) == 0) {
+		self.current_host_id = '1';
+	} else if(data.compare(Buffer.from([0xAA,0xBB,0x03,0x11,0x01,0x17])) == 0) {
+		self.current_host_id = '2';
+	} else if(data.compare(Buffer.from([0xAA,0xBB,0x03,0x11,0x02,0x18])) == 0) {
+		self.current_host_id = '3';
+	} else if(data.compare(Buffer.from([0xAA,0xBB,0x03,0x11,0x03,0x19])) == 0) {
+		self.current_host_id = '4';
+	} else if(data.compare(Buffer.from([0xAA,0xBB,0x03,0x11,0x04,0x1A])) == 0) {
+		self.current_host_id = '5';
+	} else if(data.compare(Buffer.from([0xAA,0xBB,0x03,0x11,0x05,0x1B])) == 0) {
+		self.current_host_id = '6';
+	} else if(data.compare(Buffer.from([0xAA,0xBB,0x03,0x11,0x06,0x1C])) == 0) {
+		self.current_host_id = '7';
+	} else if(data.compare(Buffer.from([0xAA,0xBB,0x03,0x11,0x07,0x1D])) == 0) {
+		self.current_host_id = '8';
+	} else if(data.compare(Buffer.from([0xAA,0xBB,0x03,0x11,0x08,0x1E])) == 0) {
+		self.current_host_id = '9';
+	} else if(data.compare(Buffer.from([0xAA,0xBB,0x03,0x11,0x09,0x1F])) == 0) {
+		self.current_host_id = '10';
+	} else if(data.compare(Buffer.from([0xAA,0xBB,0x03,0x11,0x0A,0x20])) == 0) {
+		self.current_host_id = '11';
+	} else if(data.compare(Buffer.from([0xAA,0xBB,0x03,0x11,0x0B,0x21])) == 0) {
+		self.current_host_id = '12';
+	} else if(data.compare(Buffer.from([0xAA,0xBB,0x03,0x11,0x0C,0x22])) == 0) {
+		self.current_host_id = '13';
+	} else if(data.compare(Buffer.from([0xAA,0xBB,0x03,0x11,0x0D,0x23])) == 0) {
+		self.current_host_id = '14';
+	} else if(data.compare(Buffer.from([0xAA,0xBB,0x03,0x11,0x0E,0x24])) == 0) {
+		self.current_host_id = '15';
+	} else if(data.compare(Buffer.from([0xAA,0xBB,0x03,0x11,0x0F,0x25])) == 0) {
+		self.current_host_id = '16';
+	}
+
+	self.checkFeedbacks('current_host');
+}
 
 instance_skel.extendedBy(instance);
 exports = module.exports = instance;
