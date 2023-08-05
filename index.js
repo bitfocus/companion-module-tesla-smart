c3d8a39245ef2cbc3b5ce20b4ea7757ac07352db
Julian Waller <git@julusian.co.uk>
2023-08-05 13:22:39 +0100

    feat: convert to companion3 and simplify code

--- index.js
+++ index.js
@@ -1,343 +1,267 @@
-const tcp = require('../../tcp')
-const instance_skel = require('../../instance_skel')
+// @ts-check
 
-const CHOICES_PC = [
-	{ id: '1', label: 'Source 1' },
-	{ id: '2', label: 'Source 2' },
-	{ id: '3', label: 'Source 3' },
-	{ id: '4', label: 'Source 4' },
-	{ id: '5', label: 'Source 5' },
-	{ id: '6', label: 'Source 6' },
-	{ id: '7', label: 'Source 7' },
-	{ id: '8', label: 'Source 8' },
-	{ id: '9', label: 'Source 9' },
-	{ id: '10', label: 'Source 10' },
-	{ id: '11', label: 'Source 11' },
-	{ id: '12', label: 'Source 12' },
-	{ id: '13', label: 'Source 13' },
-	{ id: '14', label: 'Source 14' },
-	{ id: '15', label: 'Source 15' },
-	{ id: '16', label: 'Source 16' },
-]
+import { InstanceBase, InstanceStatus, Regex, TCPHelper, combineRgb, runEntrypoint } from '@companion-module/base'
 
-class instance extends instance_skel {
-	init() {
-		this.current_host_id = '0'
-		this.setVariable('currentPort', '0')
-		this.polling = false
-		this.choices = CHOICES_PC.slice(0, this.config.sources)
+/**
+ * @param {number} count
+ * @returns {import('@companion-module/base').DropdownChoice[]}
+ */
+function generateChoices(count) {
+	const choices = []
 
-		this.actions()
+	for (let i = 1; i <= count; i++) {
+		choices.push({
+			id: `${i}`,
+			label: `Source ${i}`,
+		})
+	}
+
+	return choices
+}
+
+class instance extends InstanceBase {
+	async init(config) {
+		this.config = config
+
+		this.current_host_id = 0
+
+		this.initActions()
 
 		// Connect to KVM device
 		this.init_tcp()
 
 		// Start polling for feedback
-		this.start_polling_for_host()
+		this.#start_polling_for_host()
 
 		// Initialize the feedback workflow
 		this.initFeedbacks()
+
+		this.initVariables()
+		this.setVariableValues({
+			currentPort: 0,
+		})
 	}
 
 	// Reconnect to KVM after config updated, in case of IP or port change
-	updateConfig(config) {
-		if (this.socket !== undefined) {
+	async configUpdated(config) {
+		this.config = config
+
+		if (this.socket) {
 			this.socket.destroy()
 			delete this.socket
 		}
 
-		this.config = config
-		this.choices = CHOICES_PC.slice(0, this.config.sources)
-
-		this.actions()
+		this.initActions()
 		this.initFeedbacks()
-
 		this.initVariables()
 
 		this.init_tcp()
 
-		this.stop_polling_for_host()
-		this.start_polling_for_host()
+		this.#stop_polling_for_host()
+		this.#start_polling_for_host()
 	}
 
 	init_tcp() {
-		if (this.socket !== undefined) {
+		if (this.socket) {
 			this.socket.destroy()
 			delete this.socket
 		}
 
-		this.status(this.STATE_WARNING, 'Connecting')
+		this.updateStatus(InstanceStatus.Connecting)
 
 		if (this.config.host) {
-			this.socket = new tcp(this.config.host, this.config.port)
+			this.socket = new TCPHelper(this.config.host, this.config.port)
 
 			this.socket.on('status_change', (status, message) => {
-				this.status(status, message)
+				this.updateStatus(status, message)
 			})
 
 			this.socket.on('error', (err) => {
-				this.debug('Network error', err)
-				this.status(this.STATE_ERROR, err)
+				this.updateStatus(InstanceStatus.UnknownError, err.message)
 				this.log('error', 'Network error: ' + err.message)
 			})
 
 			this.socket.on('connect', () => {
-				this.status(this.STATE_OK)
-				this.debug('Connected')
+				this.updateStatus(InstanceStatus.Ok)
 			})
 
 			this.socket.on('data', (chunk) => {
 				const data = Buffer.from(chunk)
-				this.debug('Received ', data)
+				this.log('debug', 'Received ' + data.toString('hex'))
 
-				this.processData(data)
+				this.#processData(data)
 			})
 		}
 	}
 
 	// Return config fields for web config
-	config_fields() {
+	/**
+	 *
+	 * @returns {import('@companion-module/base').SomeCompanionConfigField[]}
+	 */
+	getConfigFields() {
 		return [
 			{
 				type: 'textinput',
 				id: 'host',
 				label: 'KVM IP',
 				width: 6,
-				regex: this.REGEX_IP,
+				regex: Regex.IP,
 			},
 			{
-				type: 'textinput',
+				type: 'number',
 				id: 'port',
 				label: 'Target Port',
 				width: 2,
 				default: 5000,
-				regex: this.REGEX_PORT,
+				min: 1,
+				max: 65535,
 			},
 			{
 				type: 'number',
 				id: 'sources',
 				label: '# of sources',
+				width: 6,
 				min: 1,
 				max: 16,
 				default: 8,
-				required: true,
 			},
 			{
 				type: 'number',
 				id: 'poll',
 				label: 'Feedback poll interval (ms) - 0 to disable, max 60000',
+				width: 6,
 				min: 0,
 				max: 60000,
 				default: 5000,
-				required: true,
 			},
 		]
 	}
 
 	// When module gets deleted
-	destroy() {
-		this.stop_polling_for_host()
+	async destroy() {
+		this.#stop_polling_for_host()
 
 		if (this.socket) {
 			this.socket.destroy()
 		}
 	}
 
-	actions() {
-		this.setActions({
+	initActions() {
+		const inputChoices = generateChoices(this.config.sources)
+
+		this.setActionDefinitions({
 			switch: {
-				label: 'Switch source',
+				name: 'Switch source',
 				options: [
 					{
 						type: 'dropdown',
 						id: 'id_send',
 						label: 'Source:',
 						default: '1',
-						choices: this.choices,
+						choices: inputChoices,
 					},
 				],
+				callback: async (action) => {
+					const cmd = Buffer.from([0xaa, 0xbb, 0x03, 0x01, 0x00, 0xee])
+					cmd.writeUint8(Number(action.options.id_send), 4)
+
+					await this.#sendCommand(cmd)
+				},
 			},
 		})
 	}
 
-	action(action) {
-		let cmd
+	/**
+	 * @param {Buffer} cmd
+	 */
+	async #sendCommand(cmd) {
+		this.log('debug', 'sending ' + cmd.toString('hex') + ' to ' + this.config.host)
 
-		switch (action.action) {
-			case 'switch':
-				let id = action.options.id_send
-				if (id == '1') {
-					cmd = Buffer.from([0xaa, 0xbb, 0x03, 0x01, 0x01, 0xee])
-				} else if (id == '2') {
-					cmd = Buffer.from([0xaa, 0xbb, 0x03, 0x01, 0x02, 0xee])
-				} else if (id == '3') {
-					cmd = Buffer.from([0xaa, 0xbb, 0x03, 0x01, 0x03, 0xee])
-				} else if (id == '4') {
-					cmd = Buffer.from([0xaa, 0xbb, 0x03, 0x01, 0x04, 0xee])
-				} else if (id == '5') {
-					cmd = Buffer.from([0xaa, 0xbb, 0x03, 0x01, 0x05, 0xee])
-				} else if (id == '6') {
-					cmd = Buffer.from([0xaa, 0xbb, 0x03, 0x01, 0x06, 0xee])
-				} else if (id == '7') {
-					cmd = Buffer.from([0xaa, 0xbb, 0x03, 0x01, 0x07, 0xee])
-				} else if (id == '8') {
-					cmd = Buffer.from([0xaa, 0xbb, 0x03, 0x01, 0x08, 0xee])
-				} else if (id == '9') {
-					cmd = Buffer.from([0xaa, 0xbb, 0x03, 0x01, 0x09, 0xee])
-				} else if (id == '10') {
-					cmd = Buffer.from([0xaa, 0xbb, 0x03, 0x01, 0x0a, 0xee])
-				} else if (id == '11') {
-					cmd = Buffer.from([0xaa, 0xbb, 0x03, 0x01, 0x0b, 0xee])
-				} else if (id == '12') {
-					cmd = Buffer.from([0xaa, 0xbb, 0x03, 0x01, 0x0c, 0xee])
-				} else if (id == '13') {
-					cmd = Buffer.from([0xaa, 0xbb, 0x03, 0x01, 0x0d, 0xee])
-				} else if (id == '14') {
-					cmd = Buffer.from([0xaa, 0xbb, 0x03, 0x01, 0x0e, 0xee])
-				} else if (id == '15') {
-					cmd = Buffer.from([0xaa, 0xbb, 0x03, 0x01, 0x0f, 0xee])
-				} else if (id == '16') {
-					cmd = Buffer.from([0xaa, 0xbb, 0x03, 0x01, 0x10, 0xee])
-				}
-				break
-		}
-
-		if (cmd !== undefined) {
-			this.debug('sending ', cmd, 'to', this.config.host)
-
-			if (this.socket !== undefined && this.socket.connected) {
-				this.socket.send(cmd)
-			} else {
-				this.debug('Socket not connected :(')
-			}
+		if (this.socket && this.socket.isConnected) {
+			this.socket.send(cmd)
+		} else {
+			this.log('debug', 'Socket not connected :(')
 		}
 	}
 
 	initVariables() {
 		this.setVariableDefinitions([
 			{
-				label: 'Current active HDMI port',
-				name: 'currentPort',
+				name: 'Current active HDMI port',
+				variableId: 'currentPort',
 			},
 		])
 	}
 
 	initFeedbacks() {
-		const feedbacks = {}
+		const inputChoices = generateChoices(this.config.sources)
 
-		feedbacks['current_host'] = {
-			label: 'Currently selected source',
-			description: 'Feedback showing the currently selected source',
-			options: [
-				{
-					type: 'colorpicker',
-					label: 'Background color',
-					id: 'bg_color',
-					default: this.rgb(0, 255, 0),
+		this.setFeedbackDefinitions({
+			current_host: {
+				type: 'advanced',
+				name: 'Currently selected source',
+				description: 'Feedback showing the currently selected source',
+				options: [
+					{
+						type: 'colorpicker',
+						label: 'Background color',
+						id: 'bg_color',
+						default: combineRgb(0, 255, 0),
+					},
+					{
+						type: 'dropdown',
+						label: 'Source',
+						id: 'host_id',
+						default: '1',
+						choices: inputChoices,
+					},
+				],
+				callback: (feedback) => {
+					if (Number(feedback.options.host_id) === this.current_host_id) {
+						return { bgcolor: Number(feedback.options.bg_color) }
+					} else {
+						return {}
+					}
 				},
-				{
-					type: 'dropdown',
-					label: 'Source',
-					id: 'host_id',
-					default: '1',
-					choices: this.choices,
-				},
-			],
-		}
-
-		this.setFeedbackDefinitions(feedbacks)
+			},
+		})
 	}
 
-	feedback(feedback) {
-		switch (feedback.type) {
-			case 'current_host':
-				if (feedback.options.host_id == this.current_host_id) {
-					return { bgcolor: feedback.options.bg_color }
-				}
-		}
+	#poll_for_active_host() {
+		this.log('debug', 'Polling KVM ' + this.config.host)
+
+		this.#sendCommand(Buffer.from([0xaa, 0xbb, 0x03, 0x10, 0x00, 0xee]))
 	}
 
-	poll_for_active_host() {
-		this.debug('Polling KVM ', this.config.host)
-
-		if (this.socket && this.socket.connected) {
-			this.socket.send(Buffer.from([0xaa, 0xbb, 0x03, 0x10, 0x00, 0xee]))
-		} else {
-			this.debug('Socket not connected :(')
-		}
-
-		this.checkFeedbacks('current_host')
-	}
-
-	start_polling_for_host() {
+	#start_polling_for_host() {
 		if (this.config.poll > 0) {
-			this.host_poller = setInterval(this.poll_for_active_host.bind(this), this.config.poll)
-			this.polling = true
+			this.host_poller = setInterval(this.#poll_for_active_host.bind(this), this.config.poll)
 
-			this.poll_for_active_host()
+			this.#poll_for_active_host()
+		}
+	}
+
+	#stop_polling_for_host() {
+		if (this.host_poller) {
+			clearInterval(this.host_poller)
+			delete this.host_poller
+		}
+	}
+
+	/**
+	 * @param {Buffer} data
+	 */
+	#processData(data) {
+		if (data.length === 6 && data[0] == 0xaa && data[1] == 0xbb && data[2] == 0x03 && data[3] == 0x11) {
+			this.current_host_id = data[4] + 1
+
+			this.setVariableValues({
+				currentPort: this.current_host_id,
+			})
 			this.checkFeedbacks('current_host')
 		}
 	}
-
-	stop_polling_for_host() {
-		if (this.polling) {
-			clearInterval(this.host_poller)
-		}
-	}
-
-	processData(data) {
-		if (data.compare(Buffer.from([0xaa, 0xbb, 0x03, 0x11, 0x00, 0x16])) == 0) {
-			this.current_host_id = '1'
-			this.setVariable('currentPort', '1')
-		} else if (data.compare(Buffer.from([0xaa, 0xbb, 0x03, 0x11, 0x01, 0x17])) == 0) {
-			this.current_host_id = '2'
-			this.setVariable('currentPort', '2')
-		} else if (data.compare(Buffer.from([0xaa, 0xbb, 0x03, 0x11, 0x02, 0x18])) == 0) {
-			this.current_host_id = '3'
-			this.setVariable('currentPort', '3')
-		} else if (data.compare(Buffer.from([0xaa, 0xbb, 0x03, 0x11, 0x03, 0x19])) == 0) {
-			this.current_host_id = '4'
-			this.setVariable('currentPort', '4')
-		} else if (data.compare(Buffer.from([0xaa, 0xbb, 0x03, 0x11, 0x04, 0x1a])) == 0) {
-			this.current_host_id = '5'
-			this.setVariable('currentPort', '5')
-		} else if (data.compare(Buffer.from([0xaa, 0xbb, 0x03, 0x11, 0x05, 0x1b])) == 0) {
-			this.current_host_id = '6'
-			this.setVariable('currentPort', '6')
-		} else if (data.compare(Buffer.from([0xaa, 0xbb, 0x03, 0x11, 0x06, 0x1c])) == 0) {
-			this.current_host_id = '7'
-			this.setVariable('currentPort', '7')
-		} else if (data.compare(Buffer.from([0xaa, 0xbb, 0x03, 0x11, 0x07, 0x1d])) == 0) {
-			this.current_host_id = '8'
-			this.setVariable('currentPort', '8')
-		} else if (data.compare(Buffer.from([0xaa, 0xbb, 0x03, 0x11, 0x08, 0x1e])) == 0) {
-			this.current_host_id = '9'
-			this.setVariable('currentPort', '9')
-		} else if (data.compare(Buffer.from([0xaa, 0xbb, 0x03, 0x11, 0x09, 0x1f])) == 0) {
-			this.current_host_id = '10'
-			this.setVariable('currentPort', '10')
-		} else if (data.compare(Buffer.from([0xaa, 0xbb, 0x03, 0x11, 0x0a, 0x20])) == 0) {
-			this.current_host_id = '11'
-			this.setVariable('currentPort', '11')
-		} else if (data.compare(Buffer.from([0xaa, 0xbb, 0x03, 0x11, 0x0b, 0x21])) == 0) {
-			this.current_host_id = '12'
-			this.setVariable('currentPort', '12')
-		} else if (data.compare(Buffer.from([0xaa, 0xbb, 0x03, 0x11, 0x0c, 0x22])) == 0) {
-			this.current_host_id = '13'
-			this.setVariable('currentPort', '13')
-		} else if (data.compare(Buffer.from([0xaa, 0xbb, 0x03, 0x11, 0x0d, 0x23])) == 0) {
-			this.current_host_id = '14'
-			this.setVariable('currentPort', '14')
-		} else if (data.compare(Buffer.from([0xaa, 0xbb, 0x03, 0x11, 0x0e, 0x24])) == 0) {
-			this.current_host_id = '15'
-			this.setVariable('currentPort', '15')
-		} else if (data.compare(Buffer.from([0xaa, 0xbb, 0x03, 0x11, 0x0f, 0x25])) == 0) {
-			this.current_host_id = '16'
-			this.setVariable('currentPort', '16')
-		}
-
-		this.checkFeedbacks('current_host')
-	}
 }
 
-exports = module.exports = instance
+runEntrypoint(instance, [])
